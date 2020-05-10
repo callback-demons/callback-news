c2578994ba4dedcb80345a4e7d7bad97a4df4b0a
David Behar <davbelom@gmail.com>
2020-05-10 04:56:27 -0500

    ✨ (PostItemList): Post Item List Layout

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
+import Head from 'next/head'
+import styled from 'styled-components'
+import PostItemList from '../components/PostItemList'
+import Layout from '../components/Layout'
+
+const Logo = styled.img`
+  max-width:100%;
+`
+
 function HomePage() {
   return (
-    <div>
-      <img src="/callback-news-logo.png" alt="Callback News Logo" />
-      <style jsx>
-        {`
-          div {
-            text-align: center;
-          }
-        `}
-      </style>
-    </div>
+    <Layout>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500;600;700&display=swap" rel="stylesheet" />
+      </Head>
+      <Logo src="/callback-news-logo.png" alt="Callback News Logo" />
+      <PostItemList />
+    </Layout>
   )
 }
 
