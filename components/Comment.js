075db8af6365b3e56739bd7bcb2c1d6c2c237d8a
William Velazquez <william.velazquez.a@gmail.com>
2020-05-23 23:52:48 -0500

    (Comment): Handling Responsive Design

--- components/Comment.js
+++ components/Comment.js
@@ -6,15 +6,17 @@ import Avatar from './Avatar'
 
 const CommentContainer = styled.div`
   display: flex;
-  margin: 20px auto;
+  margin: 0px auto;
   max-width: 1100px;
   align-items: center;
   width: calc(100% - 8px);
-  /* justify-content: left; */
+  @media screen and (min-width: 768px) {
+    margin: 5px auto;
+  }
 `
 
 const CommentBox = styled.div`
-  ${boxShadow}
+  /* ${boxShadow} */
   width: 100%;
   border-radius: 15px;
 `
@@ -23,6 +25,19 @@ const DataContainer = styled.div`
   width: 100%;
   display: flex;
   align-items: baseline;
+`
+
+const CustomAvatar = styled(Avatar)`
+  align-self: start;
+  margin-top: 30px;
+  max-width: 35px;
+  max-height: 35px;
+  @media screen and (min-width: 768px) {
+    margin: 8px;
+    max-width: 50px;
+    max-height: 50px;
+    align-self: center;
+  }
 `
 
 const Author = styled.p`
@@ -40,7 +55,7 @@ const PublicationDate = styled.span`
 const CommentText = styled.p`
   margin: 22px;
   margin-top: 6px;
-  font-size: ${(p) => p.theme.mobileSize}px;
+  font-size: ${(p) => p.theme.mediumSize}px;
   @media screen and (min-width: 768px) {
     font-size: ${(p) => p.theme.tabletSize}px;
   }
@@ -51,7 +66,7 @@ const Comment = (props) => {
   const date = parse('Fri, 08 May 2020 10:05:49', 'EEE, dd MMM yyyy HH:mm:ss', new Date())
   return (
     <CommentContainer>
-      <Avatar size="50px" withBorder />
+      <CustomAvatar withBorder />
       <CommentBox>
         <DataContainer>
           <Author>William Velázquez</Author>
