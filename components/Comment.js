898bfbe9bece05b9921d2369d29a5e46a8e86c84
William Velazquez <william.velazquez.a@gmail.com>
2020-05-24 21:38:52 -0500

    (Comment): Creating Skeleton / Loading State for the Comment Component

--- components/Comment.js
+++ components/Comment.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { parse, format } from 'date-fns'
-import { boxShadow } from '../styled/mixins'
+import { circle, skeletonGradient, boxShadow } from '../styled/mixins'
 import Avatar from './Avatar'
 
 const CommentContainer = styled.div`
@@ -28,10 +28,10 @@ const DataContainer = styled.div`
 `
 
 const CustomAvatar = styled(Avatar)`
-  align-self: start;
-  margin-top: 30px;
   max-width: 35px;
   max-height: 35px;
+  margin-top: 20px;
+  align-self: start;
   @media screen and (min-width: 768px) {
     margin: 8px;
     max-width: 50px;
@@ -41,35 +41,124 @@ const CustomAvatar = styled(Avatar)`
 `
 
 const Author = styled.p`
-  font-size: 1.1rem;
   font-weight: 600;
-  margin: 22px 10px 0px 22px;
+  font-size: 1.1rem;
+  margin: 15px 10px 0px 2px;
+  @media screen and (min-width: 768px) {
+    margin: 22px 10px 0px 22px;
+  }
 `
 
 const PublicationDate = styled.span`
+  margin-top: 10px;
   font-size: 0.9rem;
-  margin-top: 22px;
   font-style: italic;
+  @media screen and (min-width: 768px) {
+    margin-top: 22px;
+  }
 `
 
 const CommentText = styled.p`
-  margin: 22px;
+  margin: 10px;
   margin-top: 6px;
+  margin-left: 2px;
   font-size: ${(p) => p.theme.mediumSize}px;
   @media screen and (min-width: 768px) {
+    margin: 22px;
+    margin-top: 6px;
     font-size: ${(p) => p.theme.tabletSize}px;
   }
 `
 
+const SkeletonContainer = styled.div`
+  ${circle};
+  width: 35px;
+  padding: 3px;
+  margin-top: 5px;
+  margin-left: 6px;
+  align-self: start;
+  ${(p) => skeletonGradient(p.theme.skeleton.baseColorDark, p.theme.skeleton.shineColor, '1.5s', '-100px')};
+  @media screen and (min-width: 768px) {
+    width: 50px;
+    padding: 4px;
+    margin-top: 0px;
+    align-self: center;
+  }
+`
+
+const SkeletonInnerContainer = styled.div`
+  ${circle};
+  width: 35px;
+  height: 35px;
+  ${(p) => skeletonGradient(p.theme.skeleton.baseColor, p.theme.skeleton.shineColor, '1.5s', '-100px')};
+  @media screen and (min-width: 768px) {
+    width: 50px;
+    height: 50px;
+  }
+`
+
+const SkeletonDataContainer = styled.div`
+  width: 100%;
+  display: flex;
+  margin-bottom: 16px;
+  flex-direction: column;
+  @media screen and (min-width: 768px) {
+    margin-left: 16px;
+    margin-bottom: 22px;
+  }
+`
+
+const AuthorSkeleton = styled.div`
+  margin: 8px;
+  width: 120px;
+  height: ${(p) => p.theme.tabletSize}px;
+  ${(p) => skeletonGradient(p.theme.skeleton.baseColor, p.theme.skeleton.shineColor, '2s', '-200px')};
+  @media screen and (min-width: 768px) {
+    width: 150px;
+    height: ${(p) => p.theme.titleSize}px;
+  }
+`
+
+const TextSkeleton = styled.div`
+  margin: 8px;
+  margin-bottom: 2px;
+  width: ${(p) => p.textSize};
+  height: ${(p) => p.theme.tabletSize}px;
+  ${(p) => skeletonGradient(p.theme.skeleton.baseColor, p.theme.skeleton.shineColor, '2s', '-200px')};
+  @media screen and (min-width: 768px) {
+    height: ${(p) => p.theme.titleSize}px;
+  }
+`
+
 const Comment = (props) => {
-  const { text } = props
-  const date = parse('Fri, 08 May 2020 10:05:49', 'EEE, dd MMM yyyy HH:mm:ss', new Date())
+  const { text = '', author = 'William Velázquez', postedDate = 'Fri, 08 May 2020 10:05:49', isLoading, className } = props
+
+  if (isLoading || text === '') {
+    return (
+      <CommentContainer className={className}>
+        <SkeletonContainer>
+          <SkeletonInnerContainer />
+        </SkeletonContainer>
+        <SkeletonDataContainer>
+          <DataContainer>
+            <AuthorSkeleton />
+            <AuthorSkeleton />
+          </DataContainer>
+          <TextSkeleton textSize="90%" />
+          <TextSkeleton textSize="60%" />
+        </SkeletonDataContainer>
+      </CommentContainer>
+    )
+  }
+
+  const date = parse(postedDate, 'EEE, dd MMM yyyy HH:mm:ss', new Date())
+
   return (
-    <CommentContainer>
+    <CommentContainer className={className}>
       <CustomAvatar withBorder />
       <CommentBox>
         <DataContainer>
-          <Author>William Velázquez</Author>
+          <Author>{author}</Author>
           <PublicationDate>
             {`Posted: ${format(date, 'MM/dd/yyyy')}`}
           </PublicationDate>
