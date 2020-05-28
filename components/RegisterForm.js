bd04808ac46b5f0628af33bcc1f5d516afed3780
William Velazquez <william.velazquez.a@gmail.com>
2020-05-27 22:53:57 -0500

    (ModalRegister): Creating RegisterForm Component

---
+++ components/RegisterForm.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from 'styled-components'
+import Avatar from './Avatar'
+import LabelInput from './LabelInput'
+import Button from './Button'
+
+const MainContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-gap: 30px;
+  justify-items: center;
+`
+
+const Form = styled.form`
+  display: grid;
+  grid-gap: 20px;
+`
+
+const LinkText = styled.p`
+  margin: 0px;
+  cursor: pointer;
+  font-weight: bold;
+  text-align: center;
+  color: ${(p) => p.theme.color.secondary};
+  font-size: ${(p) => p.theme.mediumSize}px;
+  &:hover {
+    text-decoration: underline;
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 14px;
+  }
+`
+
+const RegisterForm = ({ handleSubmit = null, handleLogin = null }) => {
+  return (
+    <MainContainer>
+      <Avatar withBorder size="60px" />
+      <Form onSubmit={handleSubmit}>
+        <LabelInput
+          label="Full Name"
+        />
+        <LabelInput
+          label="Email"
+          type="email"
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2, 4}$"
+        />
+        <LabelInput
+          label="Password"
+          type="password"
+        />
+        <LabelInput
+          label="Confirm Password"
+        />
+        <Button text="Create Account" handleClick={handleSubmit} />
+        <LinkText onClick={handleLogin}>Do you have an account? Login</LinkText>
+      </Form>
+    </MainContainer>
+  )
+}
+
+export default RegisterForm
